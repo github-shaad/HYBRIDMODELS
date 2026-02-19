0e23ec48be4dba05fdeda3c2588d9259aae424dc
github-shaad <shaadhafeezofficial@gmail.com>
2026-02-19 23:53:00 +0530

    storage manager model statistics method done

--- src/main.py
+++ src/main.py
@@ -6,21 +6,24 @@ from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 from config.config import *
 from plots import MultiPlot, ResidPlot, MultiLineResidPlot, MultiLinePlot
+from storageManager import StorageManager
+
 
 train_data_path = RAW_DATA_DIR / "train_data_raw.npy"
 test_data_path = RAW_DATA_DIR / "test_data_raw.npy"
-a = np.load(train_data_path)[0:4]
-b = np.load(test_data_path)[0:4]
+a = np.load(train_data_path)[0:8]
+b = np.load(test_data_path)[0:8]
 
 arima_model = ArimaStrategy(preprocessors=[LogDiffByRow()])
 
 res = arima_model.run(a, b, True)
 
 print(f"True:{b}\nPreds:{res}")
-plot_dict = {"DUMMY": (b[0], res[0]), "DUMMY 2": (b[1], res[1]), "DUMMY 3": (b[2], res[2]), "DUMMY 4": (b[3], res[3])}
 
-p = MultiPlot("test")
 
-q = MultiLinePlot("test residuals", ["green", "blue", "black", "yellow"])
-q.plot(b[0], plot_dict, figsize=(16,9))
+r2 = r2_score(b, res)
 
+
+s = StorageManager()
+s.store_predictions(res, "model", "arima")
+s.store_statistics("model", "Rsquared", "arima", r2)
